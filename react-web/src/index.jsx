c334d120aa971e256da2604d6b5889046ff3538d
colleenchen <colleenchen@gjsoft.net>
2020-11-17 14:42:24 +0800

    一次說完 JSX 基本用法

--- react-web/src/index.jsx
+++ react-web/src/index.jsx
@@ -2,6 +2,22 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import styles from './scss/index.scss';
 
-const Main = () => <h1 className={styles.main}>Hi JSX！</h1>;
+const SayHello = (props) => {
+    const { names } = props;
+    const isEmpty = value => value === '';
+    return names.map(name => (
+        <div key={name} className={`${styles.mainBackground} ${isEmpty(name) ? '' : styles.main}`}
+            style={{
+                fontSize: 28,
+            }}>
+            {`Hello ${isEmpty(name) ? 'HAPPY' : name}`}
+        </div>
+    ));
+}
 
-ReactDom.render(<Main />, document.getElementById('root'));
+
+ReactDom.render(
+    <div>
+        <SayHello names={['World', '', 'Apple', 'Red', 'Colleen']} />
+    </div>,
+    document.getElementById('root'));
