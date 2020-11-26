df64f21bf4b6c7cf302351d74db2fbb00758ea9e
colleenchen <exdream0605@gmail.com>
2020-11-26 22:24:15 +0800

    useEffect

--- react-web/src/index.jsx
+++ react-web/src/index.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+
+import React, { useState, useEffect } from 'react';
 import ReactDom from 'react-dom';
 import styles from './scss/index.scss';
 
@@ -15,15 +16,39 @@ const SayHello = (props) => {
     ));
 }
 
-const Main = () => {
-    // 會回傳一個陣列， count 為 0 ， setCount 用來設置 count
-    const [count, setCount] = useState(0);
+const Counter = () => {
+    useEffect(() => {
+        console.log('Component Render 後執行');
+        return () => {
+          console.log('Component 移除執行');
+        }
+      }, []);
+      
+      const [count, setCount] = useState(0);
+      useEffect(() => {
+        console.log(`State 改變前 ${count}`)
+        return () => {
+          console.log(`State 改變後 ${count}`);
+        };
+      }, [count]);
     return (
         <>
             <h1 className={styles.main}>{count}</h1>
             <button type='button' onClick={() => setCount(count + 1)}>
                 點我+1
             </button>
+        </>
+    );
+}
+
+const Main  = () => {
+    const [hiddenCounter, setHiddenCounter] = useState(false);
+    return (
+        <>
+            <button type='button' onClick={() => setHiddenCounter(!hiddenCounter)}>
+                開啟關閉計時器
+            </button>
+            {hiddenCounter ? null : <Counter />}
         </>
     );
 }
