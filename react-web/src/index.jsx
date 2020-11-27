81b45e59c35e9582257e60f7424dea2da1ecf795
colleenchen <colleenchen@gjsoft.net>
2020-11-27 17:40:01 +0800

    useContext

--- react-web/src/index.jsx
+++ react-web/src/index.jsx
@@ -1,89 +1,59 @@
-
-import React, { useState, useEffect } from 'react';
+import React, {
+    useState, createContext, useContext,
+} from 'react';
 import ReactDom from 'react-dom';
 import PropTypes from 'prop-types';
-import styles from './scss/index.scss';
-const SayHello = (props) => {
-    const { names } = props;
 
-    const isEmpty = value => value === '';
-    return (
-        <div
-            className={styles.mainBackground}
-            style={{ fontSize: 28 }}
-        >
-            {names.map(name => (
-                <div
-                    key={name}
-                    className={isEmpty(name) ? '' : styles.main}
-                >
-                    {`Hello ${isEmpty(name) ? 'World' : name}`}
-                </div>
-            ))}
-        </div>
-    );
-    // return names.map(name => (
-    //     <div key={name} className={`${styles.mainBackground} ${isEmpty(name) ? '' : styles.main}`}
-    //         style={{
-    //             fontSize: 28,
-    //         }}>
-    //         {`Hello ${isEmpty(name) ? 'HAPPY' : name}`}
-    //     </div>
-    // ));
+const TodoListContext = createContext();
+
+const Task = (props) => {
+    const { task } = props;
+    return <li>{task}</li>;
 };
 
+Task.propTypes = {
+    task: PropTypes.string,
+};
 
+Task.defaultProps = {
+    task: '',
+};
 
-const Counter = () => {
-    useEffect(() => {
-        console.log('Component Render 後執行');
-        return () => {
-            console.log('Component 移除執行');
-        }
-    }, []);
+const TodoList = () => {
+    const todoList = useContext(TodoListContext);
+    return todoList.map(task => (
+        <ul key={task}>
+            <Task task={task} />
+        </ul>
+    ));
+};
 
-    const [count, setCount] = useState(0);
-    useEffect(() => {
-        console.log(`State 改變後 ${count}`)
-        return () => {
-            console.log(`State 改變前 ${count}`);
-        };
-    }, [count]);
-    return (
-        <>
-            <h1 className={styles.main}>{count}</h1>
-            <button type='button' onClick={() => setCount(count + 1)}>
-                點我+1
-            </button>
-        </>
-    );
-}
+const TodoListPage = () => (
+    <div>
+        <div>其他內容什麼的</div>
+        <TodoList />
+    </div>
+);
+
+const CurrentTask = () => {
+    const todoList = useContext(TodoListContext);
+    return <div>{`下一件事要做：${todoList[0]}`}</div>;
+};
 
 const Main = () => {
-    const [hiddenCounter, setHiddenCounter] = useState(false);
+    const [todoList] = useState(['first', 'second']);
     return (
-        <>
-            <button type='button' onClick={() => setHiddenCounter(!hiddenCounter)}>
-                開啟關閉計時器
-            </button>
-            {hiddenCounter ? <SayHello names={['World', '', 'Apple', 'Red', 'Colleen']} /> : <Counter />}
-        </>
+        <TodoListContext.Provider value={todoList}>
+            <div>
+                <span>{`代辦事項數：${todoList.length}`}</span>
+                <TodoListPage />
+                <CurrentTask />
+            </div>
+        </TodoListContext.Provider>
     );
-}
-
-
-// 定義型別為陣列
-SayHello.propTypes = {
-    names: PropTypes.arrayOf(PropTypes.string),
-};
-
-//　沒給資料預設給的字串
-SayHello.defaultProps = {
-    names: ['Default string'],
 };
 
 ReactDom.render(
-    <div>
-        <Main />
-    </div>,
-    document.getElementById('root'));
+    <Main />,
+    document.getElementById('root'),
+);
